fd0b4ad465f4f8c409f5666a8ff740edc8ccc482
saleemalfy <alal0024@algonquinlive.com>
2016-01-01 22:33:51 -0500

    updated1/1/2015
    update

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("http://i.imgur.com/stKRFUJ.jpg").innerHTML = "http://i.imgur.com/BT566Ka.jpg";
+document.getElementById("http://i.imgur.com/BT566Ka.jpg").innerHTML = "http://i.imgur.com/qVcvBpV.jpg";
+
+
+/*$(function () {
+    var body = $('body');
+    var backgrounds = [
+      'url(http://i.imgur.com/stKRFUJ.jpg)',
+      'url(http://i.imgur.com/BT566Ka.jpg)'];
+    var current = 0;
+
+    function nextBackground() {
+        body.css(
+            'background',
+            backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 50);
+    }
+    setTimeout(nextBackground, 50);
+    body.css('background', backgrounds[0]);
+});*/
